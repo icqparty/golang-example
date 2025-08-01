8bd0f4c51555fe9fe738a022660e0356fd45f9bf
Asoskov Alexander <asoskov-web@yndex.ru>
2025-08-01 17:26:17 +0300

    Refactor

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
 	"gopkg.in/yaml.v3"
 )
 
@@ -14,45 +16,73 @@ type ConfigStruct struct {
 }
 
 func readConfig(filepath string) (*ConfigStruct, error) {
-    ymlFile, err := os.ReadFile(filepath)
-    if err != nil {
-        return nil, fmt.Errorf("не удалось прочитать файл конфигурации: %w", err)
-    }
+	ymlFile, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось прочитать файл конфигурации: %w", err)
+	}
 
-    var config ConfigStruct
-    err = yaml.Unmarshal(ymlFile, &config)
-    if err != nil {
-        return nil, fmt.Errorf("не удалось распарсить YAML-файл: %w", err)
-    }
+	var config ConfigStruct
+	err = yaml.Unmarshal(ymlFile, &config)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось распарсить YAML-файл: %w", err)
+	}
 
-    return &config, nil
+	return &config, nil
 }
+func LoggerMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        
+        next.ServeHTTP(w, r)
+        
+        log.Printf(
+            "%s\t%s\t%s\t%s",
+            r.Method,
+            r.URL.Path,
+            r.RemoteAddr,
+            time.Since(start),
+        )
+    }
+}
+
+
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		fmt.Fprintf(w, "Hello, %s!", name)
+	}
 
 func main() {
 	var configPath string
-    flag.StringVar(&configPath, "f", "", "путь к файлу конфигурации")
-    flag.Parse()
+	flag.StringVar(&configPath, "f", "", "путь к файлу конфигурации")
+	flag.Parse()
 
-    // Проверяем наличие пути к файлу конфигурации
-    if len(configPath) == 0 {
-        fmt.Fprintln(os.Stderr, "Ошибка: не указан файл конфигурации.")
-        os.Exit(1)
-    }
+	// Проверяем наличие пути к файлу конфигурации
+	if len(configPath) == 0 {
+		log.Fatalf( "Не указан файл конфигурации.")
+		os.Exit(1)
+	}
 
-    // Читаем конфигурационный файл
-    config, err := readConfig(configPath)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Ошибка при чтении файла конфигурации: %v\n", err)
-        os.Exit(1)
-    }
+	// Читаем конфигурационный файл
+	config, err := readConfig(configPath)
+	if err != nil {
+		log.Fatalf("Ошибка при чтении файла конфигурации: %v :", err)
+		os.Exit(1)
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		name := r.FormValue("name")
-		fmt.Fprintf(w, "Hello, %s!", name)
-	})
-	log.Printf("Server is listening on port %s ...", config.Port)
-	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
+
+    http.HandleFunc("/", LoggerMiddleware(handlerHome))
+    http.HandleFunc("/hello", LoggerMiddleware(handlerHello))
+
+	log.Printf("Сервер запущен и слушает порт %s ...", config.Port)
+	err = http.ListenAndServe(":"+config.Port, nil)
+	if err != nil {
+		log.Fatalf("Ошибка сервера: %v\n", err)
+		os.Exit(1)
+	}
+    
+
 }
